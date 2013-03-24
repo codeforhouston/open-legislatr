c6d2f636f7f0748ae090f65e07ca2baf692efdc4
Paul Kwiatkowski <pnkwiatkowski@gmail.com>
2013-03-24 15:34:37 -0500

    Dumb/simple publisher code for demo.

--- app/clearinghouse/bill_publisher.rb
+++ app/clearinghouse/bill_publisher.rb
@@ -1,12 +1,22 @@
-class ClearingHouse::BillPublisher
+class Clearinghouse::BillPublisher
+
+  BLOG_NAME = 'openlegislatr.tumblr.com'
 
   def initialize
-    # Fetch events created in the last hour.
+    @client = Tumblr::Client.new
   end
 
 
-  def publish
+  def publish(event)
+    @client.text( BLOG_NAME,
+                  title: "Update regarding #{event.legislation.state.upcase} #{event.legislation.bill_id}",
+                  body: text_from_event(event))
     # Publish each even via each format.
+  end
+
+
+  def text_from_event(event)
+    (event.changes.map { |c| "#{c.key.titleize} has changed to #{c.to}" }).join('<br>')
   end
 
 private
